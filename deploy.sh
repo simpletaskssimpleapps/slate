242990ff6739421f956e9782196b163c0823ea29
deploy.sh <>
2020-01-22 15:04:02 -0600

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,9 @@ Options:
 # recursive build function
 run_build() {
   bundle exec middleman build --clean
-  find . -type d -name '.md' -exec sh -c "bundle exec middleman build --clean" \;
+
+
+  find . -type d -name '.md' -exec sh -c 'bundle exec middleman build --clean' \;
 }
 
 parse_args() {
